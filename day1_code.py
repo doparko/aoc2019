7bb7d109ccff17a769f393f5a2913350adef487f
dave <doparko@gmail.com>
2019-12-01 16:39:54 -0800

    day 1 fully complete and working. there is a short input for testing

--- day1_code.py
+++ day1_code.py
@@ -8,9 +8,38 @@ datin = dataopen.readlines()
 def massfuel(mass):
 	fuel = np.floor(int(mass)/3) - 2
 	return fuel
+
+def fuelfuel(mass):
+    tempmass = int(mass)
+    tots = 0
+    while (np.floor(tempmass/3) -2) > 0:
+        tots = tots + (np.floor(tempmass/3) -2)
+        tempmass = (np.floor(tempmass/3) -2)
+    return tots
+
 sum = 0
-for i in datin:
-	sum = sum + massfuel(i)
 
-print("Your total fuel needed is:",sum)
+whichpart = int(input("which part do you want to do (1/2)?:"))
+#part 1
+if whichpart == 1:
+    for i in datin:
+    	sum = sum + massfuel(i)
+    
+    print("Your total fuel needed is:",sum)
+elif whichpart ==2:
+    #part 2 here
+    for j in datin:
+        sum = sum + fuelfuel(j)
+    print("Your total fuel needed is:",sum)
+else:
+    print("bad input buddy")
+    
 
+
+def fuelfuel(mass,sums):
+    tempmass = int(mass)
+    tots = 0
+    while tempmass > 0:
+        tots = tots + (np.floor(tempmass/3) -2)
+        tempmass = (np.floor(tempmass/3) -2)
+    return tots
